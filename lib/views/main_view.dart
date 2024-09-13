8f8bfdb8410b66a1117414606ec0ec9a7ee003c5
Vg34100 <videogamer341x@gmail.com>
2024-09-13 16:02:34 -0700

    habit card

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/controllers/habit_controller.dart';
+import 'package:habit_tracker/widgets/habit_card.dart';
 
 class MainView extends StatefulWidget{
   const MainView({super.key});
@@ -10,16 +12,25 @@ class MainView extends StatefulWidget{
 }
 
 class _MainViewState extends State<MainView> {
-  @override
-  Widget build(Object context) {
-      return StreamBuilder<Object>(
-        stream: null,
-        builder: (context, snapshot) {
-          return Scaffold(
-            
-          );
-        }
-      );
-  }
+  final HabitController _habitController = HabitController();
 
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _habitController.habits.isNotEmpty
+          ? HabitCard(
+              habit: _habitController.habits[0],
+              isDone: false,
+              onDone: () {
+                // Implement your onDone logic here
+                setState(() {
+                  // Update the state if needed
+                });
+              },
+            )
+          : Center(child: Text('No habits available')),
+    );
+  }
 }
---
+++ lib/widgets/habit_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/models/habit_model.dart';
+
+class HabitCard extends StatelessWidget{
+  final Habit habit;
+  final bool isDone;
+  final VoidCallback onDone;
+
+  const HabitCard({
+    super.key,
+    required this.habit,
+    required this.isDone,
+    required this.onDone,
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+
+    return Opacity(
+      opacity: isDone ? 0.5 : 1.0,
+      child: Card(
+        child: ListTile(
+          leading: Icon(_getIconData(habit.assignedIcon)),
+          title: Text(habit.name),
+          trailing: ElevatedButton(
+            onPressed: isDone ? null : onDone,
+            child: const Text('Done'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  IconData _getIconData(String iconName) {
+    switch (iconName) {
+      case 'book':
+        return Icons.book;
+      case 'edit':
+        return Icons.edit;
+      default:
+        return Icons.check_circle;
+    }
+  }
+}
